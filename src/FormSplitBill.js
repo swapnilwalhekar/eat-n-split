561dea086de442cec150d771b47f407fa0b81e9c
Swapnil Walhekar <108648658+swapnilwalhekar@users.noreply.github.com>
2024-02-09 12:58:07 +0530

    split form bug solved

--- src/FormSplitBill.js
+++ src/FormSplitBill.js
@@ -21,13 +21,13 @@ const FormSplitBill = ({ selectedFriend, onSplitBill }) => {
       <h2>Split a bill with {selectedFriend.name}</h2>
       <label>💰 Bill value</label>
       <input
-        type="text"
+        type="number"
         value={bill}
         onChange={(e) => setBill(Number(e.target.value))}
       />
       <label>👨‍🎤 Your expense</label>
       <input
-        type="text"
+        type="number"
         value={paidByUser}
         onChange={(e) =>
           setPaidbyUser(
